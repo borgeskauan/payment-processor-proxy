a1479005d6d12720a5b4a836588395daefea8ed8
tyrael <borges.kauan.martins@gmail.com>
2025-08-10 19:41:38 -0300

    Configuring Nginx to work with Unix Sockets

--- src/main/java/borges/kauan/paymentprocessorproxy/adapter/input/UnixSocketConfig.java
+++ src/main/java/borges/kauan/paymentprocessorproxy/adapter/input/UnixSocketConfig.java
@@ -1,21 +1,23 @@
 package borges.kauan.paymentprocessorproxy.adapter.input;
 
 import io.netty.channel.unix.DomainSocketAddress;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class UnixSocketConfig {
 
-    private final String socketPath = "/tmp/spring-boot.sock";
+    @Value("${unix.socket.path:/tmp/spring-boot.sock}")
+    private String socketPath;
 
     @Bean
     public NettyReactiveWebServerFactory nettyReactiveWebServerFactory() {
         NettyReactiveWebServerFactory factory = new NettyReactiveWebServerFactory();
-        factory.addServerCustomizers(
-                httpServer -> httpServer.bindAddress(() -> new DomainSocketAddress(socketPath))
-        );
+        factory.addServerCustomizers(httpServer -> httpServer.bindAddress(() -> new DomainSocketAddress(socketPath)));
 
         return factory;
     }
