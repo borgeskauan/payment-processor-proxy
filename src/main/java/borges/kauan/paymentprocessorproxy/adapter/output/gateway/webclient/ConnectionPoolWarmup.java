afa98f87401f0e468f58e71033718e743118c1e4
tyrael <borges.kauan.martins@gmail.com>
2025-08-12 11:49:14 -0300

    Adding some logging

--- src/main/java/borges/kauan/paymentprocessorproxy/adapter/output/gateway/webclient/ConnectionPoolWarmup.java
+++ src/main/java/borges/kauan/paymentprocessorproxy/adapter/output/gateway/webclient/ConnectionPoolWarmup.java
@@ -1,5 +1,6 @@
 package borges.kauan.paymentprocessorproxy.adapter.output.gateway.webclient;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -8,6 +9,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 public class ConnectionPoolWarmup {
 
@@ -26,17 +28,19 @@ public class ConnectionPoolWarmup {
 
     @EventListener(ApplicationReadyEvent.class)
     public void warmUpPools() {
+        log.info("Warming up connection pools...");
+
         warmUp(defaultClient, DEFAULT_POOL_SIZE);
         warmUp(fallbackClient, FALLBACK_POOL_SIZE);
     }
 
     private void warmUp(WebClient client, int connectionCount) {
         Flux.range(0, connectionCount)
-            .flatMap(i -> client.get()
-                    .uri("/payments/service-health")
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .onErrorResume(e -> Mono.empty()))
-            .blockLast(); // block so pool is ready before serving traffic
+                .flatMap(i -> client.get()
+                        .uri("/payments/service-health")
+                        .retrieve()
+                        .bodyToMono(String.class)
+                        .onErrorResume(e -> Mono.empty()))
+                .blockLast(); // block so pool is ready before serving traffic
     }
 }
